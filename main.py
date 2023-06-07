546ac18ea87c6401b280e89cf54b7326f54cfca9
iknowca <iknowca@gmail.com>
2023-06-07 14:19:34 +0900

    update requirements.txt

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 # This is a sample Python script.
 
+from fastapi import FastAPI
+
 # Press ⌃R to execute it or replace it with your code.
 # Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+app = FastAPI()
 
+@app.get('/')
+def read_root():
+    return {"hello": "World"}
 
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
